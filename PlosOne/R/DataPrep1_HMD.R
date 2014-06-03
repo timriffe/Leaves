e527b673e244f2aa4d16a9d7169b59dc9c82977f
triffe <tim.riffe@gmail.com>
2014-06-03 10:51:51 -0700

    include more data for prep stage, standardize prep paths, annotate more

--- PlosOne/R/DataPrep1_HMD.R
+++ PlosOne/R/DataPrep1_HMD.R
@@ -1,116 +1,95 @@
+# change this!
+setwd("/home/triffe/git/Leaves")
 
-# 1) HMD Data Prep:
-# instructions:
-
-# 1) download the big HMD zip files from www.mortality.org
-#              1.1) lt_female.zip
-#              1.2) lt_male.zip
-#              1.3) population.zip
-# 2) extract these to a folder called /HMD/
-#    so as to create the folder structure:
-#    HMD/
-#       lt_female/
-#       lt_male/
-#       population/
-# (each of these will contain more subfolders.)
-
+# 1) 
 ## we'll need a few functions:
-# library(RiffeetalFunctions)
-# library(devtools)
-# load_all("/home/triffe/git/RiffeSpijkerMacInnes1/PlosOne/R/RiffeetalFunctions")
-
-# the main HMD folder path, change as necessary:
-HMDpath <- "/home/triffe/DATA/HMD"
-
-# slice off HMD country short codes from file names:
-HMDcountries <- unlist(lapply(list.files(file.path(HMDpath, "lt_female","fltper_1x1")), function(x){
-                    strsplit(x, split = "\\.")[[1]][[1]]
-                }))
-
+#library(RiffeetalFunctions)
+library(data.table)
+library(devtools)
+install_github("DemogBerkeley", subdir = "DemogBerkeley", username = "UCBdemography")
+install_github("Leaves", subdir = "PlosOne/R/RiffeetalFunctions", username = "timriffe")
+library(RiffeetalFunctions)
+library(DemogBerkeley)
 # -------------------------------------------------------------------------------
 # warning: the HMD part of this code takes around 15 minutes to execute
 #          on my 2009 2.26GHz laptop
+
+HMDcountries <- getHMDcountries()
+
+us      <- userInput() # INTERACTIVE! enter HMD username in console, no quotes, then press enter
+pass    <- userInput() # INTERACTIVE! enter HMD password in console, no quotes, then press enter
+
+# the rest can be naively selected and run in one pass
+Data <- do.call(rbind, lapply(HMDcountries, function(XXX,us,pass){
+                    DATm      <- readHMDweb(XXX, "mltper_1x1", username = us, password = pass)
+                    DATm$Code <- XXX
+                    DATm$Sex  <- "m"
+                    DATf      <- readHMDweb(XXX, "fltper_1x1", username = us, password = pass)
+                    DATf$Code <- XXX
+                    DATf$Sex  <- "f"
+                    POP       <- readHMDweb(XXX, "Population", username = us, password = pass)
+                    DATm$Pop1 <- POP$Male1
+                    DATm$Pop2 <- POP$Male2
+                    DATf$Pop1 <- POP$Female1
+                    DATf$Pop2 <- POP$Female2
+                    rbind(DATm, DATf)
+                }, us = us, pass = pass))
+
 # -------------------------------------------------------------------------------
-# make similar long format for HMD data, Deaths, Exp, Pop Counts all smacked together. 
-# Later we'll put on Fert data to make a single awesome object.
-Data <- do.call(rbind,lapply(HMDcountries, function(XXX, HMDpath){
-                    
-                    flt <- read.table(file.path(HMDpath,"lt_female/fltper_1x1", paste0(XXX, ".fltper_1x1.txt")),
-                            skip = 2, header = TRUE, as.is = TRUE, na.strings = ".")
-                    mlt <- read.table(file.path(HMDpath,"lt_male/mltper_1x1", paste0(XXX, ".mltper_1x1.txt")),
-                            skip = 2, header = TRUE, as.is = TRUE, na.strings = ".")
-                    pop <- read.table(file.path(HMDpath,"population/Population", paste0(XXX, ".Population.txt")),
-                            skip = 2, header = TRUE, as.is = TRUE, na.strings = ".")
-                    
-                    flt$Sex <- "f"
-                    mlt$Sex <- "m"
-                    flt$Code <- XXX
-                    mlt$Code <- XXX
-                    
-                    # Jan 1 pops
-                    pop$Year    <- as.character(pop$Year)
-                    yrs         <- sort(unique(gsub("\\+","",gsub("\\-","",pop$Year))))
-                    Nyr         <- length(yrs)
-                    
-                    pop1        <- pop[!grepl("\\-", pop$Year), ]
-                    pop2        <- pop[!grepl("\\+", pop$Year), ]
-                    pop1$Year   <- gsub("\\+","",pop1$Year)
-                    pop2$Year   <- gsub("\\-","",pop2$Year)
-                    pop1        <- pop1[pop1$Year != yrs[Nyr], ]
-                    pop2        <- pop2[pop2$Year != yrs[1], ]
-                    
-                    # we can trust the ordering here, standard HMD data and dimensions
-                    flt$Pop1    <- pop1$Female
-                    flt$Pop2    <- pop2$Female
-                    mlt$Pop1    <- pop1$Male
-                    mlt$Pop2    <- pop2$Male
-                    
-                    # return as a single obj
-                    rbind(flt, mlt)
-                }, HMDpath = HMDpath))
-
-Data$Age  <- as.integer(gsub("\\+","",Data$Age))
-Data$Year <- as.integer(Data$Year)
-
-# ------------------------------------
-
 # since we use dx, etc, lets standardize to radix 1
-Data <- do.call(rbind,lapply(split(Data, list(Data$Code, Data$Year)), function(Dat){
-                    Dat$dx <- Dat$dx / sum(Dat$dx)
-                    Dat$lx <- Dat$lx / 1e5
-                    Dat$Lx <- Dat$Lx / 1e5
-                    Dat$Tx <- Dat$Tx / 1e5
-                    Dat
-                }))
+Data$lx <- Data$lx / 1e5
+Data$Lx <- Data$Lx / 1e5
+Data$Tx <- Data$Tx / 1e5
 
-# now add Py, take a while to compute
-Data <- do.call(rbind,lapply(split(Data, list(Data$Code, Data$Sex, Data$Year)), function(Dat){
-                    if (nrow(Dat)==111){
-                        Dat$Py <- Mna0(Minf0(rowSums(Thano(Dat$Pop1, Dat$dx))))   
-                    }
-                    Dat
-                }))
+Data <- data.table(Data)
+
+# dx needs to be done in chunks
+st <- function(x){
+    x / sum(x, na.rm = TRUE)
+}
+Data$dx <- as.double(Data$dx)
+Data[, dx := st(dx), by = list(Code, Year, Sex)]
+# now dx sums to 1..
+
+# -----------------------------------
+# Py is population classified by remaining years of life (thanatological age)
+# the main goods!
+Data[,Py := 
+                Mna0(Minf0(rowSums(Thano(Pop1, dx))))   
+        ,by=list(Code,Year,Sex)]
 
 #-----------------------------------------
 rownames(Data) <- NULL
 #-----------------------------------------
+
+# for funsies, not used, we also calculate median remaining years of life by age.
+getMedianRYL <- function(lx){
+    # distribution
+    dx   <- c(-diff(lx),0)
+    # repeated in a matrix
+    DX   <- replicate(length(dx),dx)
+    # zero out earlier ages as we move up in age
+    DX[upper.tri(DX)] <- 0
+    # standardize for each age (column) to sum to 1
+    EDX  <- t(t(DX) / colSums(DX))
+    # the cumulative sum give the proportion, we want to know when it hits .5
+    EDXC <- apply(EDX,2,cumsum)
+    # approximates an exact median remaing lifespan for each age
+    MRYL <- apply(EDXC,2,function(llx){
+                if (all(is.na(llx))){
+                    return(NA)
+                }
+                splinefun(0:110~llx)(.5)
+            }) - 0:110
+    # in old ages you can get junk...
+    MRYL[is.na(MRYL) | MRYL < 0] <- 0
+    # return the goods to compare with ex.
+    MRYL
+}
+# sorry might take a while to calculate.
+Data[,MRYL := getMedianRYL(lx),by=list(Code,Year,Sex)]
+
 # save it out for later use
-# save(Data,file = "/home/triffe/git/RiffeSpijkerMacInnes1/PlosOne/Data/HMDAll.Rdata")
-#
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+save(Data,file = "PlosOne/Data/HMDAll.Rdata")
 
 
